84e2c3d0c7ae8cfd5f8503cdd70df508dbcf3a44
Rang3rDang3r <stu-evans@live.co.uk>
2019-12-19 19:38:36 +0000

    added personal carousel with pictures and styled

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,26 @@
 $(document).ready(function(){
 
+    /* Slide toggle for Index Event Text */
     $('button').click(function(){
-        $(this).prev().slideToggle('slow');
+        $(this).siblings('p').slideToggle('slow');
+    });
+
+    /* 1-5 image selector on attractions page */
+    /* change the src="" depending on each button */
+    $("#button1").click(function() {
+        $("#carousel-image").attr("src", "assets/images/carousel-images/carousel-image1.jpg")
+    });
+    $("#button2").click(function() {
+        $("#carousel-image").attr("src", "assets/images/carousel-images/carousel-image2.jpg")
+    });
+    $("#button3").click(function() {
+        $("#carousel-image").attr("src", "assets/images/carousel-images/carousel-image3.jpg")
+    });
+    $("#button4").click(function() {
+        $("#carousel-image").attr("src", "assets/images/carousel-images/carousel-image4.jpg")
+    });
+    $("#button5").click(function() {
+        $("#carousel-image").attr("src", "assets/images/carousel-images/carousel-image5.jpg")
     });
 
 })
