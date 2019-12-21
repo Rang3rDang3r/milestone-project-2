0ce0cd9202d4fc27a768676ff54ebef0c80b0d10
Rang3rDang3r <stu-evans@live.co.uk>
2019-12-21 18:53:02 +0000

    added custom marker symbols to maps api

--- assets/scripts/attractions-map.js
+++ assets/scripts/attractions-map.js
@@ -1,26 +1,62 @@
 function initMap() {
 
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 3,
+        zoom: 11,
         center: {
-            lat: 46.619261, 
-            lng: -33.134766
+            lat: 51.216123, 
+            lng: -0.797505
         }
     });
 
-    var lables = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var locations = [
-        {lat: 40.785091, lng: -73.968285},
-        {lat: 41.785091, lng: -72.968285},
-        {lat: 42.785091, lng: -71.968285}
-    ];
-
-    var markers = locations.map(function(location, i) {
-        return new google.maps.Marker({
-            position: location,
-            lable: lables[i % lables.length]
-        });
-    })
+    /* Custom markers per location */
+    var image = 'https://raw.githubusercontent.com/Concept211/Google-Maps-Markers/master/images/marker_red1.png';
+    var marker = new google.maps.Marker({
+    position: {lat: 51.219993, lng: -0.802312},
+    map: map,
+    icon: image
+    });
+    var image = 'https://raw.githubusercontent.com/Concept211/Google-Maps-Markers/master/images/marker_red2.png';
+    var marker = new google.maps.Marker({
+    position: {lat: 51.212095, lng: -0.797075},
+    map: map,
+    icon: image
+    });
+    var image = 'https://raw.githubusercontent.com/Concept211/Google-Maps-Markers/master/images/marker_red3.png';
+    var marker = new google.maps.Marker({
+    position: {lat: 51.169218, lng: -0.839272},
+    map: map,
+    icon: image
+    });
+    var image = 'https://raw.githubusercontent.com/Concept211/Google-Maps-Markers/master/images/marker_red4.png';
+    var marker = new google.maps.Marker({
+    position: {lat: 51.228912, lng: -0.732036},
+    map: map,
+    icon: image
+    });
+    var image = 'https://raw.githubusercontent.com/Concept211/Google-Maps-Markers/master/images/marker_red5.png';
+    var marker = new google.maps.Marker({
+    position: {lat: 51.183112, lng: -0.840733},
+    map: map,
+    icon: image
+    });
+    var image = 'https://raw.githubusercontent.com/Concept211/Google-Maps-Markers/master/images/marker_red6.png';
+    var marker = new google.maps.Marker({
+    position: {lat: 51.192964, lng: -0.771185},
+    map: map,
+    icon: image
+    });
+    var image = 'https://raw.githubusercontent.com/Concept211/Google-Maps-Markers/master/images/marker_red7.png';
+    var marker = new google.maps.Marker({
+    position: {lat: 51.218970, lng: -0.797906},
+    map: map,
+    icon: image
+    });
+    var image = 'https://raw.githubusercontent.com/Concept211/Google-Maps-Markers/master/images/marker_red8.png';
+    var marker = new google.maps.Marker({
+    position: {lat: 51.158006, lng: -0.794244},
+    map: map,
+    icon: image
+    });
 
     var markerCluster = new MarkerClusterer(map, markers,
         {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
